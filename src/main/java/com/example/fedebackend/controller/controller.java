0bd023d13b4ede70920cd7ed0ade342fb082e523
TALHIMOHAMMED <talhimohammed507@gmail.com>
2021-02-21 12:21:35 +0100

    commit2

--- src/main/java/com/example/fedebackend/controller/controller.java
+++ src/main/java/com/example/fedebackend/controller/controller.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @CrossOrigin
 public class controller {
@@ -18,10 +21,28 @@ public class controller {
     @GetMapping("/api/patients")
     @ResponseBody
     public ResponseEntity<?> getAllpaient(){
-      
 
          return ResponseEntity.ok(rep.findAll());
     }
+
+    @GetMapping("/api/stat")
+    @ResponseBody
+    public ResponseEntity<?> stati(){
+        Map<Integer,Integer> result = new HashMap<Integer, Integer>() ;
+        result.put(1,0);
+        result.put(0,0);
+        rep.findAll().forEach( patient -> {
+              if( patient.getSex() == 1 && patient.getTarget() == 1) {
+                  result.put(1,result.get(1)+1);
+              }
+            if( patient.getSex() == 0 && patient.getTarget() == 1) {
+                result.put(0,result.get(0)+1);
+            }
+        });
+        return ResponseEntity.ok(result);
+    }
+
+
 
 
 
